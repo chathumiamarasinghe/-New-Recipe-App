a348a643edaee684da338ff46ba51e9c9ac20888
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2024-09-26 17:46:29 +0530

    Initial commit

--- app/src/main/java/com/example/foodrecipe/MainActivity.java
+++ app/src/main/java/com/example/foodrecipe/MainActivity.java
@@ -20,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
 
+
+
         });
     }
 }
